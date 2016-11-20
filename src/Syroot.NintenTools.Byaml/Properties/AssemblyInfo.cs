8bc78709b963a54ea2b0e8c2fbf0b1552ddff0ab
Ray Koopa <raykoopa@users.noreply.github.com>
2016-11-20 18:30:31 +0100

    Fix AssemblyInfo

--- src/Syroot.NintenTools.Byaml/Properties/AssemblyInfo.cs
+++ src/Syroot.NintenTools.Byaml/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Syroot")]
 [assembly: AssemblyCopyright("WTFPL")]
 [assembly: AssemblyDescription(".NET library providing loading, saving, and serialization support for the Nintendo BYAML format.")]
-[assembly: AssemblyProduct("NintenTools.Bfres")]
-[assembly: AssemblyTitle("NintenTools.Bfres")]
+[assembly: AssemblyProduct("NintenTools.Byaml")]
+[assembly: AssemblyTitle("NintenTools.Byaml")]
 [assembly: AssemblyVersion("2.0.0.0")]
 
 [assembly: ComVisible(false)]
