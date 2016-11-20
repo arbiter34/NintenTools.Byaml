7a20ad4c2ff4fd7a571e570059b30849631bf59d
Ray Koopa <raykoopa@users.noreply.github.com>
2016-11-20 17:57:04 +0100

    Initial commit.

---
+++ src/Syroot.NintenTools.Byaml/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Syroot")]
+[assembly: AssemblyCopyright("WTFPL")]
+[assembly: AssemblyDescription(".NET library for Nintendo file formats.")]
+[assembly: AssemblyProduct("NintenTools")]
+[assembly: AssemblyTitle("NintenTools")]
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("35e9ddb2-59d4-4b41-b3cb-b659f7e0749d")]
---
+++ src/Syroot.NintenTools.Byaml/Serialization/ByamlSerializerSettings.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Syroot.NintenTools.Byaml.Serialization
+{
+    /// <summary>
+    /// Represents options to control the serialization process of a <see cref="ByamlSerializer"/>.
+    /// </summary>
+    public class ByamlSerializerSettings
+    {
+        // ---- CONSTRUCTORS & DESTRUCTOR ------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ByamlSerializerSettings"/> class.
+        /// </summary>
+        public ByamlSerializerSettings()
+        {
+            SupportPaths = true;
+            Version = ByamlVersion.Version1;
+        }
+
+        // ---- PROPERTIES ---------------------------------------------------------------------------------------------
+        
+        /// <summary>
+        /// Gets or sets a value indicating whether <see cref="IEnumerable{ByamlPathPoint}"/> instances will be
+        /// supported or expected in the BYAML file.
+        /// </summary>
+        public bool SupportPaths { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version of the BYAML file to write or expect.
+        /// </summary>
+        public ByamlVersion Version { get; set; }
+
+    }
+
+    /// <summary>
+    /// Represents the supported BYAML file versions.
+    /// </summary>
+    public enum ByamlVersion
+    {
+        /// <summary>
+        /// Represents version 1 of the BYAML file format.
+        /// </summary>
+        Version1 = 1
+    }
+}
