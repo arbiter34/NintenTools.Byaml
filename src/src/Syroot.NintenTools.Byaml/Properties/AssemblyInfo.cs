c7274eaaa1402c358433a24afe7b7aa12dfeb13e
Ray Koopa <raykoopa@users.noreply.github.com>
2017-04-18 16:38:31 +0200

    Relicensed under MIT. Updated repository structure.

---
+++ src/src/Syroot.NintenTools.Byaml/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Syroot")]
+[assembly: AssemblyCopyright("WTFPL")]
+[assembly: AssemblyDescription(".NET library providing loading, saving, and serialization support for the Nintendo BYAML format.")]
+[assembly: AssemblyProduct("NintenTools.Byaml")]
+[assembly: AssemblyTitle("NintenTools.Byaml")]
+[assembly: AssemblyVersion("2.0.0.0")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("35e9ddb2-59d4-4b41-b3cb-b659f7e0749d")]
